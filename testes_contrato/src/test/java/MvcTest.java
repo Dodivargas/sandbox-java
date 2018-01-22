30c15e814e4da4708a6a495798874cf630221810
Alex Rocha <alex.rochas@yahoo.com.br>
2018-01-22 16:45:42 -0200

    fix: json error

--- testes_contrato/src/test/java/MvcTest.java
+++ testes_contrato/src/test/java/MvcTest.java
@@ -1,12 +1,24 @@
 import app.endpoint.Endpoint;
+import app.service.Service;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class MvcTest {
+
+    @Mock
+    Service service;
+    @InjectMocks
+    Endpoint endpoint;
 
     @Before
     public void setup() {
-        RestAssuredMockMvc.standaloneSetup(new Endpoint());
+        System.out.println(endpoint);
+        RestAssuredMockMvc.standaloneSetup(endpoint);
     }
 
 }
